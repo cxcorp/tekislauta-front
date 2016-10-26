8989edd66ea6daf25f01888766442d42ceb72331
Hugo Holmqvist <hugo.holm@live.com>
2016-10-26 17:41:22 +0300

    Add thread view

--- src/Thread.js
+++ src/Thread.js
@@ -1,15 +1,46 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Post from './Post';
 
 class Thread extends Component {
-  /* PARAMS: props.params.abbreviation, props.params.thread */
-  render() {
+    /* PARAMS: props.params.abbreviation, props.params.thread */
+    constructor(props) {
+        super(props);
 
-    return (
-      <div className='Thread'>
-        Threads not implemented yet
-      </div>
-    );
-  }
+        this.state = {
+            threads: null
+        };
+    }
+    render() {
+        let threads = [];
+        if (this.state.threads) {
+            threads = this.state.threads.map(thread => <Post data={thread} key={thread.id} board={this.props.abbreviation}/>);
+        }
+        return (
+            <div className='Thread'>
+                <p>
+                    {threads}
+                </p>
+            </div>
+        );
+    }
+
+    fetchPosts() {
+        fetch('/api/boards/' + this.props.abbreviation + '/posts/' + this.props.id).then(res => {
+            if (!res.ok)
+                console.log('Therad::fetchPosts', res);
+            res.json().then(res => {
+                this.setState({
+                    threads: res.data
+                });
+            }).catch(e => {
+                console.log(e);
+            });
+        });
+    }
+
+    componentDidMount() {
+        this.fetchPosts();
+    }
 }
 
 export default Thread;
